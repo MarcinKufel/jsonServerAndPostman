475b24b009f46c303333c9e0b3d348375615f4eb
Marcin <marcinkufel84@gmail.com>
2021-07-26 07:24:16 +0200

    added new test

--- cypress/integration/post-request.js
+++ cypress/integration/post-request.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Post Request", () => {
+let titleOfPosts = new Array();
 
     it("Create a new post via /posts api", () => {
         cy.request({
@@ -14,4 +15,23 @@ describe("Post Request", () => {
             expect(response.status).to.eql(201)
         })
     });
+
+    it("Validate title of latest post", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:3000/posts",
+            body: {
+                title: "Want to learn automation testing?",
+                author: "Sarah Jones"
+            }
+        }).then(response => {
+            let body = JSON.parse(JSON.stringify(response.body));
+            body.forEach(function(item) {
+                titleOfPosts.pust(item["title"]);
+            })
+        }).then(() => {
+            let latestPost = titleOfPosts[titleOfPosts.length-1]
+            expect(latestPost).to.eq("Want to learn automation testing?")
+        })
+    })
 });
